90173ea60104303a8c9d407e65225f994fdbb1ff
AmitReuveni <amitreuveni@gmail.com>
2023-07-08 21:59:22 +0300

    added reference to score and timer texts and basic methods to operate them

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DefaultNamespace;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Button goButton;
+    private TMP_Text goButtonText;
+    private Image goButtonImage;
     [SerializeField] private PickupSpawner pickupSpawner;
     [SerializeField] private DropoffSpawner dropoffSpawner;
+
+    [SerializeField] private TMP_Text timerText;
+    [SerializeField] private TMP_Text scoreText;
 
     [SerializeField] private Material[] colorMaterials; // 0 is red, 1 is blue, 2 is green
 
@@ -15,6 +23,9 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject dropoffMarkerPrefab;
     
     [SerializeField] private int numPickups = 3;
+    
+    [SerializeField] private int timerSeconds = 72;
+    private int score = 0;
 
     private List<(Node, Node)> PickupDropoffNodesList;
     
@@ -27,10 +38,14 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         PickupDropoffNodesList = new List<(Node, Node)>();
+        goButtonText = goButton.GetComponentInChildren<TMP_Text>();
+        goButtonImage = goButton.GetComponent<Image>();
     }
 
     private void Start()
     {
+        DisableGoButton();
+        SetTimerText(timerSeconds);
         List<Node> pickups = pickupSpawner.SpawnCurrentPickupPoints(numPickups);
         List<Node> dropoffs = dropoffSpawner.SpawnCurrentDropOffPoints(numPickups);
         foreach (var node in pickups)
@@ -60,5 +75,46 @@ public class GameManager : MonoBehaviour
             
             materialIndex++;
         }
+    }
+
+    private void DisableGoButton()
+    {
+        goButtonText.text = "Set complete delivery course";
+        goButtonText.fontSize = 18;
+        goButton.interactable = false;
+        var tempColor = goButtonImage.color;
+        tempColor.a = 0.5f;
+        goButtonImage.color = tempColor;
+    }
+
+    private void EnableGoButton()
+    {
+        goButtonText.text = "Go";
+        goButtonText.fontSize = 22;
+        goButton.interactable = true;
+        var tempColor = goButtonImage.color;
+        tempColor.a = 1f;
+        goButtonImage.color = tempColor;
+    }
+
+    public void SetTimerText(int seconds)
+    {
+        int minutes = seconds / 60;
+        int onlySeconds = seconds - (minutes * 60);
+        string extraZero = onlySeconds < 10 ? "0" : "";
+        
+        timerText.text = $"{minutes}" + ":" + extraZero + $"{onlySeconds}";
+    }
+
+    public void AddScore(int addScore)
+    {
+        score += addScore;
+        scoreText.text = score.ToString();
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
     }
 }

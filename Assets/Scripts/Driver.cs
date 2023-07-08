93849cfa571db140312f7a46d04d3f7925646bdd
AmitReuveni <amitreuveni@gmail.com>
2023-07-08 18:06:06 +0300

    Updated driver drive to destination coroutine

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -6,10 +6,18 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] private Node currentNode;
     [SerializeField] private Node currentDestinationNode;
+    
+    [SerializeField] private float moveSpeed = 0.002f;
 
     public Node GetCurrentNode()
     {
         return currentNode;
+    }
+
+    private void Start()
+    {
+        transform.position = currentNode.transform.position;
+        StartCoroutine(DriveToDestinationNodeCoroutine());
     }
 
     public void SetDestinationNode(Node destNode)
@@ -17,14 +25,21 @@ public class Driver : MonoBehaviour
         currentDestinationNode = destNode;
     }
 
-    public IEnumerator DriveToDestinationNodeCoroutine(float seconds)
+
+    public IEnumerator DriveToDestinationNodeCoroutine()
     {
-        for (float time = 0 ; time < seconds ; time += Time.deltaTime )
-        {
-            transform.position = Vector3.Lerp( currentNode.transform.position, currentDestinationNode.transform.position, Mathf.SmoothStep( 0, 1, time ) );
+
+        while(transform.position != currentDestinationNode.transform.position) {
+ 
+            // move driver towards the destination, never moving farther than "moveSpeed" in one frame.
+            transform.position = Vector2.MoveTowards(transform.position, currentDestinationNode.transform.position, moveSpeed);
+ 
+            // wait until next frame to continue
+            yield return null;
         }
 
         currentNode = currentDestinationNode;
+        Debug.Log("reached destination: " + currentNode.ToString());
         yield return null;
     }
 }

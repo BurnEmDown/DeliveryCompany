b819eda117ff5c708e12600da1618423289c5039
AmitReuveni <amitreuveni@gmail.com>
2023-07-08 18:14:46 +0300

    Added reference to dropoff spawner script in game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private PickupSpawner pickupSpawner;
+    [SerializeField] private DropoffSpawner dropoffSpawner;
 
     private void Start()
     {
         List<Node> pickups = pickupSpawner.SpawnCurrentPickupPoints(2);
+        List<Node> dropoffs = dropoffSpawner.SpawnCurrentDropOffPoints(2);
         foreach (var node in pickups)
         {
             Debug.Log("pickup point: " + node.ToString());
+        }
+        foreach (var node in dropoffs)
+        {
+            Debug.Log("dropoff point: " + node.ToString());
         }
     }
 }

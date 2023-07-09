4d547ac464eebe98440a4e41bd75eb36cb776fb3
AmitReuveni <amitreuveni@gmail.com>
2023-07-09 18:36:14 +0300

    Added pause and reset buttons

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using DefaultNamespace;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -19,6 +20,10 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private Button goButton;
     [SerializeField] private Button resetRouteButton;
+    [SerializeField] private Button pauseButton;
+    [SerializeField] private TMP_Text pauseButtonText;
+    [SerializeField] private Button resetButton;
+    [SerializeField] private Image pauseImage;
     private TMP_Text goButtonText;
     private Image goButtonImage;
     [SerializeField] private PickupSpawner pickupSpawner;
@@ -34,14 +39,16 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private GameObject pickupMarkerPrefab;
     [SerializeField] private GameObject dropoffMarkerPrefab;
-    
+
     [SerializeField] private int numPickups = 3;
-    
+
     [SerializeField] private int timerSeconds = 72;
     private int score = 0;
 
     public List<Marker> selectedMarkers;
     public List<Path> selectedPaths;
+
+    private bool isPaused;
 
     // needed for create primitive
     private MeshFilter meshFilter;
@@ -59,7 +66,7 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this);
         }
-        
+
         goButtonText = goButton.GetComponentInChildren<TMP_Text>();
         goButtonImage = goButton.GetComponent<Image>();
     }
@@ -73,6 +80,7 @@ public class GameManager : MonoBehaviour
     }
 
     int currentMatchIndex = 0;
+
     public void CreatePickupsAndDropoffs(int num)
     {
         var pickupDropoffNodesList = new List<(Node, Node)>();
@@ -118,7 +126,7 @@ public class GameManager : MonoBehaviour
             GameObject foodIconPickup = Instantiate(foodIcon, pickupObject.transform);
             foodIconPickup.transform.localPosition = Vector3.back + (Vector3.up * 0.9f);
             foodIconPickup.transform.localScale *= 3f;
-            
+
             GameObject foodIconDropoff = Instantiate(foodIcon, dropoffObject.transform);
             foodIconDropoff.transform.localPosition = Vector3.back + (Vector3.up * 0.65f);
             foodIconDropoff.transform.localScale *= 0.8f;
@@ -153,7 +161,7 @@ public class GameManager : MonoBehaviour
     {
         resetRouteButton.interactable = true;
     }
-    
+
     private void DisableResetRouteButton()
     {
         resetRouteButton.interactable = false;
@@ -164,7 +172,7 @@ public class GameManager : MonoBehaviour
         int minutes = seconds / 60;
         int onlySeconds = seconds - (minutes * 60);
         string extraZero = onlySeconds < 10 ? "0" : "";
-        
+
         timerText.text = minutes + ":" + extraZero + onlySeconds;
     }
 
@@ -194,7 +202,7 @@ public class GameManager : MonoBehaviour
         CalculateAndAddPathToMarker(marker);
         EnableResetRouteButton();
 
-        if (selectedMarkers.Count == numPickups)
+        if (selectedMarkers.Count >= numPickups)
         {
             EnableGoButton();
         }
@@ -212,6 +220,7 @@ public class GameManager : MonoBehaviour
             marker.label.text = (i + 1).ToString();
         }
     }
+
     public void RedrawAvailabilityColors()
     {
         var allMarkers = FindObjectsOfType<Marker>();
@@ -227,6 +236,7 @@ public class GameManager : MonoBehaviour
                 opaqueSelectedColor.a = 1;
                 color = Color.Lerp(color, opaqueSelectedColor, selectedColorStrength);
             }
+
             if (!marker.IsValidDestination())
             {
                 var disableColorStrength = disabledButtonColor.a;
@@ -262,13 +272,46 @@ public class GameManager : MonoBehaviour
 
     public void ResetRoute()
     {
-        if(selectedPaths != null)
+        if (selectedPaths != null)
             selectedPaths.Clear();
-        if(selectedMarkers != null)
+        if (selectedMarkers != null)
             selectedMarkers.Clear();
         RedrawNumbersOnMarkers();
         //RedrawAvailabilityColors();
         DisableResetRouteButton();
         DisableGoButton();
     }
+
+    public void OnPauseButtonClick()
+    {
+        if (!isPaused)
+        {
+            isPaused = true;
+            pauseImage.gameObject.SetActive(true);
+            pauseButtonText.text = "Unpause";
+            resetButton.gameObject.SetActive(true);
+            Time.timeScale = 0f;
+            DisableResetRouteButton();
+            DisableGoButton();
+        }
+        else
+        {
+            isPaused = false;
+            pauseImage.gameObject.SetActive(false);
+            pauseButtonText.text = "Pause";
+            resetButton.gameObject.SetActive(false);
+            Time.timeScale = 1f;
+            if (selectedMarkers.Count > 0)
+                EnableResetRouteButton();
+            if (selectedMarkers.Count >= numPickups)
+                EnableGoButton();
+        }
+
+    }
+
+    public void OnResetButtonClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
 }

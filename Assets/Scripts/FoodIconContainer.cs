6d75fb08d9e4b410b30bf113e2eacda3d9c865a1
AmitReuveni <amitreuveni@gmail.com>
2023-07-09 13:12:18 +0300

    Added foodIconContainer script that chooses food icons randomly without repetition

---
+++ Assets/Scripts/FoodIconContainer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class FoodIconContainer : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> foodIconList;
+
+        private List<GameObject> availableFoodIconList;
+        private List<GameObject> recentlyChosenFoodIconList;
+
+        public GameObject GetRandomAvailableFoodIcon()
+        {
+            int index = Random.Range(0, availableFoodIconList.Count);
+            GameObject currentIcon = availableFoodIconList[index];
+            recentlyChosenFoodIconList.Add(currentIcon);
+            availableFoodIconList.RemoveAt(index);
+            if (availableFoodIconList.Count <= 3)
+            {
+                RefreshList();
+            }
+            recentlyChosenFoodIconList.Add(currentIcon);
+            return currentIcon;
+        }
+
+        private void RefreshList()
+        {
+            availableFoodIconList.AddRange(recentlyChosenFoodIconList);
+            recentlyChosenFoodIconList.Clear();
+        }
+    }
+}
